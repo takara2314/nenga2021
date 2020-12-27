a1515d2e3740be341946bc15fdf6e9b45dbcf111
takara2314 <takara2314_TK@outlook.jp>
2020-12-27 15:31:43 +0900

    😊 Include sample CG

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 func main() {
 	router := gin.Default()
 
+	// HTMLテンプレートの読み込み (同じディレクトリで実行する扱い)
+	router.LoadHTMLGlob("./*.html")
+	// 静的ファイルの読み込み
+	router.Static("/resources", "./resources")
+
 	router.GET("/", rootGET)
 	router.GET("/ar", arGET)
 	router.NoRoute(noRoute)
@@ -17,14 +22,17 @@ func main() {
 	router.Run(":" + os.Getenv("PORT"))
 }
 
+// [GET] /
 func rootGET(c *gin.Context) {
 	c.String(http.StatusOK, "こんにちは、世界！")
 }
 
+// [GET] /ar
 func arGET(c *gin.Context) {
-	c.String(http.StatusOK, "ARの世界へようこそ！")
+	c.HTML(200, "AR.html", nil)
 }
 
+// 404
 func noRoute(c *gin.Context) {
 	c.String(http.StatusNotFound, "見つかりませんでした。")
 }
