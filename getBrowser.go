81e496bcfbe4d3deecf12e137e11379a394edc8f
takara2314 <takara2314_TK@outlook.jp>
2020-12-28 21:58:53 +0900

    🎇 Fix getBrowser process for macOS

--- getBrowser.go
+++ getBrowser.go
@@ -5,13 +5,13 @@ import "strings"
 // ユーザーエージェントからブラウザ名を取得
 func getBrowser(agent string) string {
 	if strings.Contains(agent, "Macintosh") || strings.Contains(agent, "iPhone") || strings.Contains(agent, "iPad") || strings.Contains(agent, "iPod") {
-		if strings.Contains(agent, "CriOS") {
+		if strings.Contains(agent, "CriOS") || strings.Contains(agent, "Chrome") {
 			return "Chrome"
-		} else if strings.Contains(agent, "FxiOS") {
+		} else if strings.Contains(agent, "FxiOS") || strings.Contains(agent, "Firefox") {
 			return "Firefox"
-		} else if strings.Contains(agent, "EdgiOS") {
+		} else if strings.Contains(agent, "EdgiOS") || strings.Contains(agent, "Edge") {
 			return "Edge"
-		} else if strings.Contains(agent, "OPT") {
+		} else if strings.Contains(agent, "OPT") || strings.Contains(agent, "Opera") {
 			return "Opera"
 		} else if strings.Contains(agent, "Line") {
 			return "LINE"
