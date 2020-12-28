146aff8744010ecd58f058623dcb566773082118
takara2314 <takara2314_TK@outlook.jp>
2020-12-28 20:50:49 +0900

    🎋 Fix getBrowser for iOS/iPadOS

--- getBrowser.go
+++ getBrowser.go
@@ -4,7 +4,23 @@ import "strings"
 
 // ユーザーエージェントからブラウザ名を取得
 func getBrowser(agent string) string {
-	if strings.Contains(agent, "MSIE") {
+	if strings.Contains(agent, "AppleWebKit") {
+		if strings.Contains(agent, "CriOS") {
+			return "Chrome"
+		} else if strings.Contains(agent, "FxiOS") {
+			return "Firefox"
+		} else if strings.Contains(agent, "EdgiOS") {
+			return "Edge"
+		} else if strings.Contains(agent, "OPT") {
+			return "Opera"
+		} else if strings.Contains(agent, "Line") {
+			return "LINE"
+		} else if strings.Contains(agent, "YJApp-IOS") {
+			return "Yahoo! JAPAN"
+		} else {
+			return "Safari"
+		}
+	} else if strings.Contains(agent, "MSIE") {
 		return "Internet Explorer"
 	} else if strings.Contains(agent, "Edge") {
 		return "Edge"
@@ -12,8 +28,6 @@ func getBrowser(agent string) string {
 		return "Chrome"
 	} else if strings.Contains(agent, "Firefox") {
 		return "Firefox"
-	} else if strings.Contains(agent, "Safari") {
-		return "Safari"
 	} else if strings.Contains(agent, "Opera") {
 		return "Opera"
 	} else if strings.Contains(agent, "Sleipnir") {
