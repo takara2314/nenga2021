ab36aa12e19fec643b7bd293d5a40dd13c449dec
takara2314 <takara2314_TK@outlook.jp>
2020-12-31 21:10:47 +0900

    🦴 Clear git cache

--- resources/js/load_typeface.js
+++
@@ -1,9 +0,0 @@
-if ( !window['lightbox_js_dynamic'] ) {
-	window.lightbox_js_dynamic = {};
-}
-if ( !window.lightbox_js_dynamic.typeface ) {
-	window.lightbox_js_dynamic.typeface = true;
-	window.lightbox_js_dynamic.str="";
-	window.lightbox_js_dynamic.str+="<"+"script type=\"text/javascript\" src=\"./resources/js/typeface-0.15.js\"></"+"script> ";
-	document.write(window.lightbox_js_dynamic.str);
-}
